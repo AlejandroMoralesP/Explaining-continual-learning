602aa11b8d09342eea9a837f88a3272a97bf2845
Alejandro <alejandro.morales.pe@gmail.com>
2020-08-06 19:20:35 +0100

    first steps in lacunarity development

---
+++ lacunarity.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+def lacunarity2(img):
+    for i in range(len(a)):
+        for j in range(len(a[0])):
+            print(img[i][j])
+    
+
+a = np.zeros(shape=(6,6))
+
+var = 0
+for i in range(len(a)):
+    for j in range(len(a[0])):
+        var = var + 1
+        a[i][j] = var
+
+
+lacunarity2(a)
+
+

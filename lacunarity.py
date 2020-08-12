3245e1d7619c7a87f9f191915c64dacdc0b03cd7
Alejandro <alejandro.morales.pe@gmail.com>
2020-08-12 03:08:12 +0100

    changes in lacunarity2 function

--- lacunarity.py
+++ lacunarity.py
@@ -1,9 +1,16 @@
 import numpy as np
 
 def lacunarity2(img):
-    for i in range(len(a)):
-        for j in range(len(a[0])):
-            print(img[i][j])
+    count = 0
+
+    for i in range(0,len(img),2):
+        for j in range(0,len(img[0]),2):
+            for k in range(i,i+2,1):
+                for l in range(j,j+2,1):
+                    if img[k][l] == 1:
+                        count = count + 1
+            print("Cuadrado con {} unos".format(count))
+            count = 0
     
 
 a = np.zeros(shape=(6,6))
@@ -11,7 +18,10 @@ a = np.zeros(shape=(6,6))
 var = 0
 for i in range(len(a)):
     for j in range(len(a[0])):
-        var = var + 1
+        if i % 2 == 1:
+            var = 1
+        else:
+            var = 0
         a[i][j] = var
 
 

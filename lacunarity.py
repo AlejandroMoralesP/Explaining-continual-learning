e2297c9c52ae5dbf071ee4ddb78dc7ceceb09f25
Alejandro <alejandro.morales.pe@gmail.com>
2020-08-26 15:08:07 +0100

    finished versions of lacunarity2 and lacunaritygreater

--- lacunarity.py
+++ lacunarity.py
@@ -28,7 +28,7 @@ def lacunarity2(img):
                             if img[k][l] != img[k-1][l]: count += 1
                             if img[k][l] != img[k][l-1]: count += 1
 
-            print("Square with {} different neighbors".format(count))
+            print("Box with {} different neighbors".format(count))
             if count == 0:
                 totals[0] = totals[0]+1
             elif count == 6:
@@ -48,17 +48,103 @@ def lacunarity2(img):
     lacunarity = m2/m1**2
     print(lacunarity)
 
+
+def lacunaritygreater(img, sz):
+    if sz < 3:
+        exit()
+    
+    count = 0
+    totals = [0] * 9
+
+    for i in range(0,len(img),sz):
+        for j in range(0,len(img[0]),sz):
+            for k in range(i,i+sz,1):
+                for l in range(j,j+sz,1):
+                    if k == i:
+                        if l == j:
+                            if img[k][l] != img[k][l+1]: count += 1
+                            if img[k][l] != img[k+1][l]: count += 1
+                            if img[k][l] != img[k+1][l+1]: count += 1
+                        elif l == j+sz-1:
+                            if img[k][l] != img[k][l-1]: count += 1
+                            if img[k][l] != img[k+1][l-1]: count += 1
+                            if img[k][l] != img[k+1][l]: count += 1
+                        else:
+                            if img[k][l] != img[k][l-1]: count += 1
+                            if img[k][l] != img[k+1][l-1]: count += 1
+                            if img[k][l] != img[k+1][l]: count += 1
+                            if img[k][l] != img[k+1][l+1]: count += 1
+                            if img[k][l] != img[k][l+1]: count += 1
+                    elif k == i+sz-1:
+                        if l == j:
+                            if img[k][l] != img[k-1][l]: count += 1
+                            if img[k][l] != img[k-1][l+1]: count += 1
+                            if img[k][l] != img[k][l+1]: count += 1
+                        elif l == j+sz-1:
+                            if img[k][l] != img[k-1][l-1]: count += 1
+                            if img[k][l] != img[k-1][l]: count += 1
+                            if img[k][l] != img[k][l-1]: count += 1
+                        else:
+                            if img[k][l] != img[k][l-1]: count += 1
+                            if img[k][l] != img[k-1][l-1]: count += 1
+                            if img[k][l] != img[k-1][l]: count += 1
+                            if img[k][l] != img[k-1][l+1]: count += 1
+                            if img[k][l] != img[k][l+1]: count += 1
+                    else:
+                        if l == j:
+                            if img[k][l] != img[k+1][l]: count += 1
+                            if img[k][l] != img[k+1][l+1]: count += 1
+                            if img[k][l] != img[k][l+1]: count += 1
+                            if img[k][l] != img[k-1][l+1]: count += 1
+                            if img[k][l] != img[k-1][l]: count += 1
+                        elif l == j+sz-1:
+                            if img[k][l] != img[k+1][l]: count += 1
+                            if img[k][l] != img[k+1][l-1]: count += 1
+                            if img[k][l] != img[k][l-1]: count += 1
+                            if img[k][l] != img[k-1][l-1]: count += 1
+                            if img[k][l] != img[k-1][l]: count += 1
+                        else:
+                            if img[k][l] != img[k-1][l-1]: count += 1
+                            if img[k][l] != img[k-1][l]: count += 1
+                            if img[k][l] != img[k-1][l+1]: count += 1
+                            if img[k][l] != img[k][l-1]: count += 1
+                            if img[k][l] != img[k][l+1]: count += 1
+                            if img[k][l] != img[k+1][l-1]: count += 1
+                            if img[k][l] != img[k+1][l]: count += 1
+                            if img[k][l] != img[k+1][l+1]: count += 1
+
+                    for m in range(0,9):
+                        if count == m:
+                            totals[m] = totals[m]+1
+
+                    count = 0
+
+    mult1 = 0
+    mult2 = 0
+
+    for i in range(0,9):
+        print("Squares with {} different neighbors: {}".format(i, totals[i]))
+        mult1 += i * totals[i]
+        mult2 += i**2 * totals[i]
+
+    print("{} {}".format(mult1, mult2))
+    m1 = (1/(len(img)*len(img))) * mult1
+    m2 = (1/(len(img)*len(img))) * mult2
+    
+    lacunarity = m2/m1**2
+    print(lacunarity)
+
 a = np.zeros(shape=(6,6))
 
 var = 0
 for i in range(len(a)):
     for j in range(len(a[0])):
-        a[i][j] = random.randint(1,2)
+        a[i][j] = random.randint(1,3)
         print(a[i][j])
     
     print("")
 
 
-lacunarity2(a)
+lacunaritygreater(a, 3)
 
 
